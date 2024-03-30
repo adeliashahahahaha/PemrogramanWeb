b9586a7d751263565af759032ba09c8515801141
adeliashahahahaha <asha.syrn5@gmail.com>
2024-03-30 12:22:42 +0700

    Sat/30-3-24 Pertemuan 8

---
+++ Jobsheet 8/form_upload.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>File Upload</title>
+    </head>
+    <body>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload File" name="Submit">
+        </form>
+    </body>
+</html>
+
